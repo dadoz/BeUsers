9e31e15e471a49b78d8ebb2dc079aae4851da4bb
davide.lamanna <davide.la-manna@vodafone.com>
2021-03-12 12:31:45 +0100

    added night mode version

--- app/src/main/java/com/davidelmn/application/frenzspots/spotList/SpotListFragment.kt
+++ app/src/main/java/com/davidelmn/application/frenzspots/spotList/SpotListFragment.kt
@@ -1,5 +1,6 @@
 package com.davidelmn.application.frenzspots.spotList
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,17 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.davidelmn.application.frenzspots.R
 import com.davidelmn.application.frenzspots.databinding.SpotListFragmentBinding
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import timber.log.Timber
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SpotListFragment : Fragment() {
+class SpotListFragment : Fragment(), OnMapReadyCallback {
     private val spotViewModel by lazy {
         ViewModelProvider(this).get(SpotListViewModel::class.java)
     }
@@ -44,8 +51,35 @@ class SpotListFragment : Fragment() {
             }
         })
 
+        (childFragmentManager.findFragmentByTag("mapFragmentTag") as SupportMapFragment).getMapAsync(
+            this
+        )
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_SpotListFragment_to_NewSpotFragment)
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success =
+                googleMap?.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        context,
+                        R.raw.style_json
+                    )
+                )
+            if (success == false) {
+                Timber.e(SpotListFragment::class.simpleName, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Timber.e(SpotListFragment::class.simpleName, "Can't find style. Error: $e")
+        }
+
+        val cameraPosition = CameraPosition.Builder()
+            .target(LatLng(45.0703, 7.6869)).zoom(15f).build()
+        googleMap?.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition))
     }
 }
